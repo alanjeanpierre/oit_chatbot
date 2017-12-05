4746691ddc2175ac88ca6b8dac0968cc1d1f51f2
alanjeanpierre <ajeanpierre825@gmail.com>
2017-12-05 00:07:24 -0800

    Added simple text processor and tests

---
+++ oit_chatbot/text_processor.py
@@ -0,0 +1,36 @@
+from textblob import TextBlob
+
+NOUN_LIST = {
+    'blackboard',
+    'asap',
+    'degreeworks',
+}
+
+def find_verb(sentence):
+    pass
+
+def find_object(sentence):
+    objects = noun_phraser(sentence)
+    #objects = sentence.noun_phrases
+    return objects
+
+def find_question(sentence):
+    for word, part_of_speech in sentence.pos_tags:
+        if part_of_speech.startswith('W'):
+            return word
+
+def noun_phraser(sentence):    
+    nouning = False
+    noun_phrases = []
+    nouns = []
+    for word, part_of_speech in sentence.pos_tags:
+        if nouning and not (part_of_speech.startswith(('NN', 'JJ', )) or word.lower() in NOUN_LIST):
+            nouning = False
+            noun_phrases.append(' '.join(nouns))
+            nouns = []
+        if part_of_speech.startswith(('NN', 'JJ', )) or word.lower() in NOUN_LIST:
+            nouning = True
+            nouns.append(word)
+    if nouns:
+        noun_phrases.append(' '.join(nouns))
+    return noun_phrases
---
+++ tests/test_text_processor.py
@@ -0,0 +1,35 @@
+import pytest
+
+from oit_chatbot.text_processor import *
+from textblob import TextBlob
+
+qs = {
+    'Where is blackboard?' : ('where', ['blackboard']),
+    'How do I upload grades?' : ('how', ['upload grades']),
+    'When is the due date for financial aid?' : ('when', ['due date', 'financial aid']),
+    'When is the due date for tuition?' : ('when', ['due date', 'tuition']),
+    'When is the last day to drop classes?' : ('when', ['last day', 'classes']),
+    'When are my final exams?': ('when', ['final exams']),
+    'What is the phone number for tech support?': ('what', ['phone number',  'tech support']),
+    'Where is Dr. Niu\'s office?': ('where', ['dr. niu',  'office']),
+    'What are Dr. Niu\'s office hours?': ('what', ['dr. niu', 'office hours']),
+    'What is Dr. Niu\'s phone number?': ('what', ['dr. niu', 'phone number']),
+    'What is Dr. Niu\'s email address?': ('what', ['dr. niu', 'email address']),
+    'Where is degreeworks?': ('where', ['degreeworks']),
+}
+    
+
+    
+
+
+def test_questions():
+    """Questions should be parsed correctly"""
+    
+    for question, answer in qs.items():
+        q_word = find_question(TextBlob(question.lower()))
+        assert q_word.lower() == answer[0]
+
+def test_objects():
+    for question, answer in qs.items():
+        obj = find_object(TextBlob(question.lower()))
+        assert obj == answer[1]
