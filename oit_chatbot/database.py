8ac47428bbbf0018d81e6892a9eae888de4692e4
Alan Jeanpierre <ajeanpierre825@gmail.com>
2017-12-07 15:49:15 -0600

    add better incorrect question handling

--- oit_chatbot/database.py
+++ oit_chatbot/database.py
@@ -3,7 +3,7 @@ import sqlite3
 def find_question(db, noun_phrases):
     
     answers = []
-    
+    print(noun_phrases)
     if not noun_phrases:
         raise LookupError('No noun phrases found')
     elif len(noun_phrases) == 1:
@@ -11,12 +11,14 @@ def find_question(db, noun_phrases):
                             (noun_phrases))
         answers = cursor.fetchall()
     else:
+        print(noun_phrases[-1])
+        print(noun_phrases[-2])
         cursor = db.execute('select answer from knowledge where topic = ? and qualifier = ?', 
                             ( noun_phrases[-1], noun_phrases[-2]) )
         answers = cursor.fetchall()
         if not answers: # nothing with that qualifier, try just the topic
             cursor = db.execute('select answer from knowledge where topic = ?', 
-                            (noun_phrases[-1]))
+                            (noun_phrases[-1],)) # comma is necessary to make it a tuple and not an enclosed statement
     
     if not answers:
         raise LookupError('No answers for that question')
