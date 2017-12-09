fb66ca8158c717a853e4b5914c4a2a3ba9dc84a4
Alan Jeanpierre <ajeanpierre825@gmail.com>
2017-12-09 12:47:01 -0600

    Change base sql statements

--- oit_chatbot/knowledge.sql
+++ oit_chatbot/knowledge.sql
@@ -1,6 +1,6 @@
 insert into knowledge (topic, qualifier, answer) values 
     ('blackboard', NULL, 'https://utsa.blackboard.com'),
-    ('grades', NULL, 'You can view your grades on either blackboard or ASAP'),
+    ('grade', NULL, 'You can view your grades on either blackboard or ASAP'),
     ('financial aid', 'due date', 'You missed the due date for financial aid'),
     ('tuition', 'due date', 'You missed the due date and UTSA dropped your classes'),
     ('class', 'last day', 'Classes will never end'),
