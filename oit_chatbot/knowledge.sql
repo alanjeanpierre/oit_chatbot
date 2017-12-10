c9f37662f5aa12de08d2b20505da68f259ee10aa
Alan Jeanpierre <ajeanpierre825@gmail.com>
2017-12-10 11:04:16 -0600

    Change database questions

--- oit_chatbot/knowledge.sql
+++ oit_chatbot/knowledge.sql
@@ -1,5 +1,5 @@
 insert into knowledge (topic, qualifier, answer) values 
-    ('blackboard', NULL, 'https://utsa.blackboard.com'),
+    ('blackboard', NULL, 'Navigate to https://utsa.blackboard.com'),
     ('grade', NULL, 'You can view your grades on either blackboard or ASAP'),
     ('financial aid', 'due date', 'You missed the due date for financial aid'),
     ('tuition', 'due date', 'You missed the due date and UTSA dropped your classes'),
@@ -8,7 +8,8 @@ insert into knowledge (topic, qualifier, answer) values
     ('oit', 'phone number', '1-800-YOU-FAIL'),
     ('office', 'dr niu', 'Her office is located everywhere you arent'),
     ('office hour', 'dr niu', 'Her office hours are only when youre in class'),
-    ('phone number', 'dr niu', 'She only communicates vio the secure messaging app, not telephone'),
+    ('phone number', 'dr niu', 'She only communicates via the secure messaging app, not telephone'),
     ('degreeworks', NULL, 'Find degreeworks through asap'),
-    ('class', null, 'What about classes?');
+    ('class', null, 'What about classes?')
+    ;
     
